3039d3f20128d220d1b46f1a33f5a244f2d0ba09
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-08 16:44:31 +0100

    edited app.js

--- bonus/assets/js/app.js
+++ bonus/assets/js/app.js
@@ -5,38 +5,35 @@ createApp({
             randomEmailUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
             errorMsg: null,
             emails: [],
-            tempArray: [],
         }
     },
     methods: {
-        generateEmails(){
-            for(let i = 0; i < 10; i++){
+        generateEmails() {
+            for (let i = 0; i < 10; i++) {
                 //console.log(axios)
+                //console.log('prima di axios', i);
                 axios.get(this.randomEmailUrl)
-                .then(response => {
-                    //console.log(response.data.response)
-                    this.tempArray.push(response.data.response);
-                    //console.log(this.emails)
-                })
-                .catch(error => {
-                    this.errorMsg = error.message;
-                })
+                    .then(response => {
+                        //console.log(response.data.response)
+                        this.emails.push(response.data.response);
+                        //console.log('dentro axios', i);
+                    })
+                    .catch(error => {
+                        this.errorMsg = error.message;
+                    })
+
             }
-           //console.log(this.emails)
-           
-          
+            //console.log(this.emails)
+            //console.log(tempArray)
+
+
         },
-        wait(time) {
-            setTimeout(()=>{
-                this.emails = this.tempArray;
-            }, time);
-            
-            
-        }
-    },
-    mounted(){
-        this.generateEmails();
-        this.wait(1000);
         
+    },
+    mounted() {
+        //console.log(this.generateEmails())
+        this.generateEmails();
+
+
     }
 }).mount('#app')
