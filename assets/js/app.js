0bddcb57490b5ad9df7fca6ddc5b98d2eda3afbd
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-08 13:40:44 +0100

    generating emails and display li

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,26 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
     data() {
         return {
-            
+            randomEmailUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            emails: [],
         }
+    },
+    methods: {
+        generateEmails(){
+            for(let i = 0; i < 10; i++){
+                //console.log(axios)
+                axios.get(this.randomEmailUrl)
+                .then(response => {
+                    //console.log(response.data.response)
+                    this.emails.push(response.data.response);
+                    //console.log(this.emails)
+                })
+            }
+           //console.log(this.emails)
+        }
+    },
+    mounted(){
+        this.generateEmails();
+        //console.log(this.emails)
     }
 }).mount('#app')
